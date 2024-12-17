d1b5706d29ee6fde01ca36f6454be8e7a4a50044
pfvlz <73408957+pfvlz@users.noreply.github.com>
2024-12-17 13:32:24 +0800

    Update ele_ncjsb.py

--- ele_ncjsb.py
+++ ele_ncjsb.py
@@ -56,7 +56,7 @@ class LYB:
 
         try:
             r = requests.post(
-                "sign接口",
+                "http://192.168.1.253:9999/api/getXSign",
                 json=body
             )
             r.raise_for_status()
